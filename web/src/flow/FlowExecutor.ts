7cc72adb60b4f890f898348e5e36bc7ffb7dc0c4
comma0517 <comma0517@gmail.com>
2023-10-11 16:05:32 +0000

    Add GTM Track code

--- web/src/flow/FlowExecutor.ts
+++ web/src/flow/FlowExecutor.ts
@@ -489,23 +489,9 @@ export class FlowExecutor extends Interface implements StageHost {
 
     render(): TemplateResult {
         return html`
-        <!-- Google Tag Manager --> 
-        <script>
-            (function(w,d,s,l,i){w[l]=w[l]||[];
-                w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-                var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-                j.async=true;
-                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5G28KCLJ');
-        </script>
-        <!-- End Google Tag Manager -->
         <ak-locale-context>
             <div class="pf-c-background-image">${this.renderBackgroundOverlay()}</div>
-            <div class="pf-c-page__drawer">
-            <!-- Google Tag Manager (noscript) -->
-            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5G28KCLJ" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-            <!-- End Google Tag Manager (noscript) -->    
+            <div class="pf-c-page__drawer">  
                 <div class="pf-c-drawer ${this.inspectorOpen ? "pf-m-expanded" : "pf-m-collapsed"}">
                     <div class="pf-c-drawer__main">
                         <div class="pf-c-drawer__content">
